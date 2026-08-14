15a1802c3fc509b24fd29d380a48a90af0483592
Pedro157240 <peedrohrk@gmail.com>
2026-08-14 08:29:35 -0300

    Update server to use environment variable for port

--- backend/server.js
+++ backend/server.js
@@ -187,6 +187,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-app.listen(3000, () => {
-  console.log("Divino Hospital rodando em http://localhost:3000");
+const PORT = process.env.PORT
+            || 3000;
+app.listen(PORT, () => {
+  console.log(`Porta ${PORT}`);
 });
